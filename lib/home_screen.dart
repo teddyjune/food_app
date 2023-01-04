8f04fe3d90f5cdf0cb47875cd69ef9d3bf7fac0a
yeonjoo.lee <teddyjune@naver.com>
2023-01-04 19:14:56 +0900

    feat: ImageCarousel의 현재 페이지 알려주는 dot 작동구현

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -39,24 +39,52 @@ class HomeScreen extends StatelessWidget {
               )
             ],
           ),
-          SliverToBoxAdapter(
-              child: AspectRatio(
-            aspectRatio: 1.81,
-            child: Stack(
-              alignment: Alignment.bottomRight,
-              children: [
-                PageView.builder(
-                    itemCount: demoBigImages.length,
-                    itemBuilder: (context, index) =>
-                        Image.asset(demoBigImages[index])),
-                const Positioned(
-                  bottom: 10,
-                  right: 40,
-                  child: IndicatorDot(),
+          const SliverToBoxAdapter(child: ImageCarousel()),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageCarousel extends StatefulWidget {
+  const ImageCarousel({Key? key}) : super(key: key);
+
+  @override
+  State<ImageCarousel> createState() => _ImageCarouselState();
+}
+
+class _ImageCarouselState extends State<ImageCarousel> {
+  int currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.81,
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          PageView.builder(
+              itemCount: demoBigImages.length,
+              onPageChanged: (value) {
+                setState(() {
+                  currentPage = value;
+                });
+              },
+              itemBuilder: (context, index) =>
+                  Image.asset(demoBigImages[index])),
+          Positioned(
+            bottom: 10,
+            right: 40,
+            child: Row(
+              children: List.generate(
+                demoBigImages.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(left: defaultPadding / 4),
+                  child: IndicatorDot(isActive: index == currentPage),
                 ),
-              ],
+              ),
             ),
-          )),
+          ),
         ],
       ),
     );
@@ -64,16 +92,18 @@ class HomeScreen extends StatelessWidget {
 }
 
 class IndicatorDot extends StatelessWidget {
-  const IndicatorDot({Key? key}) : super(key: key);
+  final bool isActive;
+
+  const IndicatorDot({Key? key, required this.isActive}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 4,
       width: 8,
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(12))),
+      decoration: BoxDecoration(
+          color: isActive ? Colors.white : Colors.white54,
+          borderRadius: const BorderRadius.all(Radius.circular(12))),
     );
   }
 }
